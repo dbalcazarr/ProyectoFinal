127e0670f5a78423167a58bbb3982baa770a2efe
25AngelTorres <25AngelTorres@gmail.com>
2014-07-21 23:55:28 -0500

    Proyecto Final

--- views/site/inicio.php
+++ views/site/inicio.php
@@ -11,8 +11,8 @@
                 <div class="row">
 
                     <div class="col-md-8 col-md-offset-2">
-                        <h1 class="brand-heading">Progra Web</h1>
-                        <p class="intro-text">Proyecto final de programaci&oacute;n web</p>
+                        <h1 class="brand-heading">Bienvenido</h1>
+                        <p class="intro-text">Proyecto final de programaci&oacute;n web, revista electr&oacute;nica con vistas publicas y privadas, login para poder administrar la base de datos y hacer inserciones en ella.</p>
                         <div class="page-scroll">
                             <a href="#consultas" class="btn btn-circle">
                                 <i class="fa fa-angle-double-down animated"></i>
